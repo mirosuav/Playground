ce3aacdde8c0e3b498ecf293f88815d044cd4b7b
Mirosław Miedziarek <miedziarek@outlook.com>
2023-10-04 01:14:48 +0200

    wip

--- Source/LanguageExtensions/Monads/Option.cs
+++ Source/LanguageExtensions/Monads/Option.cs
@@ -9,16 +9,8 @@ namespace LanguageExtensions.Monads;
 /// <typeparam name="TX">Type of contained object</typeparam>
 public record struct Option<TX>
 {
-    public static readonly Option<TX> None = default;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Option<TX> Create(TX? value)
-        => TypeCheck<TX>.IsNull(value) ? None : new(value!);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Option<TX> Some(TX value) => new(value);
-
     public readonly bool IsSome;
+
     internal readonly TX Value;
 
     private Option(TX value)
@@ -28,11 +20,20 @@ public record struct Option<TX>
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public Option<TY> Map<TY>(Func<TX, TY> mapper)
-        => IsSome ? Option<TY>.Create(mapper(Value)) : Option<TY>.None;
+    public Option<TY> Map<TY>(Func<TX, TY> mapper) => IsSome ? Option<TY>.Create(mapper(Value)) : Option<TY>.None;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public TX Reduce(TX @default) => IsSome ? Value : @default;
+
+
+
+    public static readonly Option<TX> None = default;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Option<TX> Create(TX? value) => TypeCheck<TX>.IsNull(value) ? None : new(value!);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Option<TX> Some(TX value) => new(value);
 
 }
 
---
+++ Source/LanguageExtensions/Monads/ResultOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageExtensions.Monads;
+
+/// <summary>
+/// Represents a result from a method that can either be 
+/// Option value or exception
+/// </summary>
+public class ResultOption
+{
+}
