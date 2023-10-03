4684112b9909e463c7531488c95f1387836be4ce
Mirosław Miedziarek <miedziarek@outlook.com>
2023-10-03 22:54:42 +0200

    Option

---
+++ Source/LanguageExtensions/Monads/Option.cs
@@ -0,0 +1,35 @@
+﻿using LanguageExtensions.Objects;
+using System.Runtime.CompilerServices;
+
+namespace LanguageExtensions.Monads;
+
+public record struct Option<X>
+{
+    private readonly bool IsSome;
+    private readonly X Value;
+    internal Option(X value)
+    {
+        Value = value;
+        IsSome = true;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Option<Y> Map<Y>(Func<X, Y> mapper)
+        => IsSome
+        ? Option<Y>.Create(mapper(Value))
+        : Option<Y>.None;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public X Reduce(X @default) => IsSome ? Value : @default;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Option<X> Create(X value)
+        => TypeCheck<X>.IsNull(value)
+        ? None
+        : Some(value);
+
+    public static readonly Option<X> None = default;
+    public static Option<X> Some(X value) => new(value);
+}
+
+
---
+++ Source/LanguageExtensions/Objects/TypeCheck.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace LanguageExtensions.Objects;
+internal static class TypeCheck<T>
+{
+    private static readonly bool IsReferenceType;
+    private static readonly bool IsNullableType;
+
+    static TypeCheck()
+    {
+        //T is Nullable type
+        IsNullableType = Nullable.GetUnderlyingType(typeof(T)) is not null;
+        //T is reference type
+        IsReferenceType = !typeof(T).IsValueType;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static bool IsNull(T value) =>
+        (IsReferenceType && value is null) || (IsNullableType && value.Equals(default));
+}
