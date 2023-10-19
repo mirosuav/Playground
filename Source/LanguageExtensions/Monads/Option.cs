32f85a6a766cb8736501d3b3283ea012671db35d
Mirosław Miedziarek <miroslaw.miedziarek@eidias.pl>
2023-10-19 17:56:20 +0200

    Extensions and corrections

--- Source/LanguageExtensions/Monads/Option.cs
+++ Source/LanguageExtensions/Monads/Option.cs
@@ -1,48 +1,76 @@
 ﻿using LanguageExtensions.Objects;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace LanguageExtensions.Monads;
 
+
 /// <summary>
 /// Optional object monad. Supports reference types, Value types and Nullable types.
 /// </summary>
-/// <typeparam name="X">Type of contained object</typeparam>
-public record struct Option<X>
+/// <typeparam name="T">Type of contained object</typeparam>
+[DebuggerStepThrough]
+public record struct Option<T>
 {
-    public static readonly Option<X> None = default;
+    private const string NoneString = "None";
 
-    private readonly X _value;
+    /// <summary>
+    /// Indicates none/empty value
+    /// </summary>
+    public static readonly Option<T> None = default;
+
+    private readonly T _value;
     private readonly bool _isSome;
 
+    /// <summary>
+    /// Access raw value. It can be null.
+    /// </summary>
+    public T Value => _value;
+
+    /// <summary>
+    /// Has non-null value
+    /// </summary>
     public bool IsSome => _isSome;
+
+    /// <summary>
+    /// IS empty/none
+    /// </summary>
     public bool IsNone => !_isSome;
-    private Option(X value, bool isSome) => (_value, _isSome) = (value, isSome);
+    private Option(T value, bool isSome) => (_value, _isSome) = (value, isSome);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Option<T> Create<T>(T value) => TypeCheck<T>.IsNull(value) ? Option<T>.None : new Option<T>(value, true);
+    public static Option<TType> Create<TType>(TType value)
+        => TypeCheck<TType>.IsNull(value) ? Option<TType>.None : new Option<TType>(value, true);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Option<T> Create<T>(T? value) where T : struct
-        => value.HasValue ? Option<T>.Create(value.Value) : Option<T>.None;
+    public static Option<TStruct> Create<TStruct>(TStruct? value) where TStruct : struct
+        => value.HasValue ? Option<TStruct>.Create(value.Value) : Option<TStruct>.None;
 
     /// <summary>
     /// Creates Option with value but that is signaled as None
     /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static Option<T> Artificial<T>(T value) => new Option<T>(value, false);
+    internal static Option<T> Artificial(T value) => new Option<T>(value, false);
 
     /// <summary>
     /// Call provided mapper with concrete X value if instance is Some<X>, returns None<Y> otherwise.
     /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public Option<Y> Map<Y>(Func<X, Y> mapper) => IsSome ? Option<Y>.Create(mapper(_value)) : Option<Y>.None;
+    public Option<Y> Map<Y>(Func<T, Y> mapper) => IsSome ? Option<Y>.Create(mapper(_value)) : Option<Y>.None;
 
     /// <summary>
     /// Extract encapsulated value if instance is Some<X>, returns provided default otherwise.
     /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public X Reduce(X @default = default!) => IsSome ? _value : @default;
+    public T Reduce(T @default = default!) => IsSome ? _value : @default;
 
+    /// <summary>
+    /// Reduce value is exists and cast it to target type <typeparamref name="TCast"/>. Return None<<typeparamref name="TCast"/>> is fails.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public TCast ReduceAs<TCast>(TCast @default = default!) => IsSome && _value is TCast outVal ? outVal : @default;
 
+    public override string? ToString() => IsSome ? _value!.ToString() : NoneString;
 
+    public static implicit operator Option<T>(T value) => Create(value);
 }
--- Source/LanguageExtensions/Monads/OptionExtensions.cs
+++ Source/LanguageExtensions/Monads/OptionExtensions.cs
@@ -1,50 +1,61 @@
-﻿using System.Runtime.CompilerServices;
+﻿using LanguageExtensions.Exceptions;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace LanguageExtensions.Monads;
 
+[DebuggerStepThrough]
 public static partial class Option
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Option<T> None<T>() => Option<T>.None;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Option<T> For<T>(T value) => Option<T>.Create(value);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Option<T> For<T>(T? value) where T : struct => Option<T>.Create(value);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Option<T> ToOption<T>(this T value) => Option<T>.Create(value);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Option<T> ToOption<T>(this T? value) where T : struct => Option<T>.Create(value);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Option<T> None<T>() => Option<T>.None;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Option<T> For<T>(T value) => value.ToOption();
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Option<T> For<T>(T? value) where T : struct => value.ToOption();
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Option<T> ToOption<T>(this T value) => Option<T>.Create(value);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Option<T> ToOption<T>(this T? value) where T : struct => Option<T>.Create(value);
 
     /// <summary>
-    /// Tests the Optional value for condition and throws or returns None when condition is not met.
+    /// Map Option<T> to Option<Y> only if provided condition is met. Returns None<Y> otherwise
     /// </summary>
-    public static Option<T> Test<T>(this Option<T> option, Predicate<T> predicate, bool throwOnFalse = false)
+    public static Option<Y> MapIf<T, Y>(this Option<T> option, Func<T, bool> condition, Func<T, Y> mapper)
     {
-        if (option.IsSome && predicate(option.Reduce()))
+        if (option.IsSome)
         {
-            if (throwOnFalse)
-                throw new ApplicationException($"Option<{typeof(T).Name}> condition failed.");
-            else
-                return Option.None<T>();
+            var val = option.Reduce();
+            if (condition(val))
+                return Option<Y>.Create(mapper(val));
         }
+        return Option<Y>.None;
+    }
+
+    /// <summary>
+    /// Tests the Optional value for condition and throws when condition is met otherwise returns original Option. For None option skips testing and returns.
+    /// </summary>
+    public static Option<T> ThrowIf<T>(this Option<T> option, Func<T, bool> throwCondition)
+    {
+        if (option.IsSome && throwCondition(option.Reduce()))
+            throw NotSupportedOptionalException.Instance;
 
         return option;
     }
 
+    /// <summary>
+    /// Returns None<T> when provided condition is met otherwise returns original Option
+    /// </summary>
+    public static Option<T> NoneIf<T>(this Option<T> option, Func<T, bool> noneCondition)
+    {
+        if (option.IsSome && noneCondition(option.Reduce()))
+            None<T>();
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        return option;
+    }
+
     public static void Do<T>(this Option<T> option, Action<T> action)
     {
         if (option.IsSome)
             action(option.Reduce());
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ValueTask Do<T>(this Option<T> option, Func<T, ValueTask> action)
     {
         if (option.IsSome)
@@ -53,7 +64,6 @@ public static partial class Option
         return ValueTask.CompletedTask;
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Task Do<T>(this Option<T> option, Func<T, Task> action)
     {
         if (option.IsSome)
