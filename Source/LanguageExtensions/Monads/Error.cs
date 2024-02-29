fbe1a1d9339877d1bda36a08f0f981b028e730a6
Mirek <40789254+mirosuav@users.noreply.github.com>
2024-02-29 12:20:32 +0100

    optional Result added

---
+++ Source/LanguageExtensions/Monads/Error.cs
@@ -0,0 +1,29 @@
+﻿
+namespace LanguageExtensions.Monads;
+
+/// <summary>
+/// Operation Error
+/// </summary>
+/// <param name="Code">Short error code</param>
+/// <param name="Description">Error human readable description</param>
+/// <param name="ErrorType">Error type</param>
+public record Error(string Code, string Description, ErrorType ErrorType)
+{
+    public static readonly Error None = Failure(string.Empty, string.Empty);
+    public static readonly Error NullValue = Failure("Error.NullValue", "Null or missing value provided.");
+    public static readonly Error ServerError = Failure("Error.Server", "Internal server error");
+    public static readonly Error OperationCancelled = Failure("Error.Cancelled", "Operation cancelled");
+
+    public static Error Failure(string code, string description) => new(code, description, ErrorType.Failure);
+    public static Error Validation(string code, string description) => new(code, description, ErrorType.Validation);
+    public static Error NotFound(string code, string description) => new(code, description, ErrorType.NotFound);
+    public static Error Conflict(string code, string description) => new(code, description, ErrorType.Conflict);
+}
+
+public enum ErrorType
+{
+    Failure = 0,
+    Validation = 1,
+    NotFound = 2,
+    Conflict = 3
+}
