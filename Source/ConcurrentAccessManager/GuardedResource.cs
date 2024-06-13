1604d3364dca0b3c61a21862e32920e11f0a4f98
Mirek Miedziarek <miedziarek@outlook.com>
2024-06-13 22:44:20 +0200

    Resource guard

---
+++ Source/ConcurrentAccessManager/GuardedResource.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentAccessManager;
+
+
+internal class GuardedResource
+{
+    public static string[] Resources = ["Resource 1", "Resource 2", "Resource 3", "Resource 4"];
+
+    private static readonly ConcurrentDictionary<string, (int, int)> _resources =
+        new ConcurrentDictionary<string, (int, int)>(new Dictionary<string, (int, int)>
+        {
+            { "Resource 1", (0,100) },
+            { "Resource 2", (0,100) },
+            { "Resource 3", (0,100) },
+            { "Resource 4", (0,100) }
+        });
+
+    private readonly SessionManager _sessionManager = new();
+
+
+    public async Task<Guid?> RequestResourceUsage(string resourceName, int requestedQuantity)
+    {
+        await _sessionManager.LockAsync(resourceName);
+
+        try
+        {
+            Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} books {resourceName} with quantity of {requestedQuantity}.");
+
+            var limit = GetResourceLimit(resourceName);
+
+            if (limit > 0
+                && GetResourcePersistedUsage(resourceName)
+                    + _sessionManager.GetBookedCount(resourceName)
+                    + requestedQuantity
+                    > limit)
+            {
+                Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} exceeds {resourceName} limit: {limit}.");
+                return null;
+            }
+
+            var sessionId =  _sessionManager.NewSession(resourceName, requestedQuantity);
+
+            Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} acquired session: {sessionId} for resource {resourceName} with quantity of {requestedQuantity}.");
+           
+            return sessionId;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+        finally
+        {
+            _sessionManager.Unlock(resourceName);
+        }
+    }
+
+    private int GetResourcePersistedUsage(string resourceName) =>
+        _resources.TryGetValue(resourceName, out var r)
+        ? r.Item1
+        : 0;
+
+    private int GetResourceLimit(string resourceName) =>
+        _resources.TryGetValue(resourceName, out var r)
+        ? r.Item2
+        : 0;
+
+}
+
---
+++ Source/ConcurrentAccessManager/SessionManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConcurrentAccessManager;
+
+public sealed class SessionManager
+{
+    private class ResourceSessions
+    {
+        private readonly Dictionary<Guid, int> _sessions = new();
+        public SemaphoreSlim Semaphore { get; } = new(1, 1);
+
+        public int BookedCount => _sessions.Sum(x => x.Value);
+
+        public Guid Add(int requestedQuantity)
+        {
+            var id = Guid.NewGuid();
+            _sessions.Add(id, requestedQuantity);
+            return id;
+        }
+
+        public bool Remove(Guid id)
+        {
+            return _sessions.Remove(id);
+        }
+    }
+
+    private readonly static ConcurrentDictionary<string, ResourceSessions> _sessions = new();
+
+    public int GetBookedCount(string resourceName)
+    {
+        return _sessions.TryGetValue(resourceName, out var sessions)
+            ? sessions.BookedCount
+            : 0;
+    }
+
+    public Task LockAsync(string resourceName)
+    {
+        return _sessions
+            .GetOrAdd(resourceName, new ResourceSessions())
+            .Semaphore
+            .WaitAsync();
+    }
+
+    public int Unlock(string resourceName)
+    {
+        return _sessions
+            .GetOrAdd(resourceName, new ResourceSessions())
+            .Semaphore
+            .Release();
+    }
+
+    public Guid NewSession(string resourceName, int requestedQuantity)
+    {
+        return _sessions
+            .GetOrAdd(resourceName, new ResourceSessions())
+            .Add(requestedQuantity);
+    }
+
+    public void Commit(string resourceName, Guid Id)
+    {
+        var removed = _sessions.TryGetValue(resourceName, out var sessions)
+            ? sessions.Remove(Id)
+            : false;
+
+        if (!removed)
+            throw new InvalidOperationException($"No session with Id: {Id} was registered for resource: {resourceName}.");
+    }
+}
